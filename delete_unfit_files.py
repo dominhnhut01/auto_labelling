a63d306f6b94f7458cf5f22f0262f91c3844740f
dominhnhut01 <dominhnhut01@gmail.com>
2021-06-22 14:10:10 +0700

    Change repo name

--- delete_unfit_files.py
+++ delete_unfit_files.py
@@ -16,13 +16,27 @@ def delete(ref_dir,processed_dir):
 		ref_files[i] = os.path.splitext(ref_files[i])[0]
 	print(ref_files)
 	for file in os.listdir(processed_dir):
-		file, ext = os.path.splitext(file)
-		if file not in ref_files:
-			try:
-				os.remove('{}/{}'.format(processed_dir, file + ext))
-				print('Done')
-			except:
-				continue
+		# file, ext = os.path.splitext(file)
+		# if file in ref_files:
+		# 	try:
+		# 		os.remove('{}/{}'.format(processed_dir, file + ext))
+		# 		print('Done')
+		# 	except:
+		# 		continue
+		#read input file
+		fin = open('{}/{}'.format(processed_dir, file), "rt")
+		#read file contents to string
+		data = fin.read()
+		#replace all occurrences of the required string
+		data = data.replace('0 ', '1 ')
+		#close the input file
+		fin.close()
+		#open the input file in write mode
+		fin = open('{}/{}'.format(processed_dir, file), "wt")
+		#overrite the input file with the resulting data
+		fin.write(data)
+		#close the file
+		fin.close()
 
 if __name__ == '__main__':
 
